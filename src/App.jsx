90be6a65482cad0abda3c7a6ab24577f8bf3c3ed
BaileyKH <106282001+BaileyKH@users.noreply.github.com>
2024-03-27 12:55:35 -0700

    initialized Nav Bar and React Router

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,17 @@
-
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { NavBar } from './Components/NavBar/NavBar';
+import { Home } from './Pages/Home';
 
 function App() {
 
   return (
     <>
+    <Router>
+      <NavBar />
+      <Routes>
+        <Route exact path ="/" element={<Home />} />
+      </Routes>
+    </Router>
     </>
   )
 }
