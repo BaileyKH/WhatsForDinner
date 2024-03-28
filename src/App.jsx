cd565d8febc8fc4f2df9d299f69e0dc1d386acb7
BaileyKH <106282001+BaileyKH@users.noreply.github.com>
2024-03-28 11:13:33 -0700

    added nav bar elements and nav routes

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,8 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
 import { NavBar } from './Components/NavBar/NavBar';
 import { Home } from './Pages/Home';
+import { RecipeDetail } from './Pages/RecipeDetail';
+import { DietaryRestrictions } from './Pages/DietaryRestrictions';
 
 function App() {
 
@@ -11,6 +13,8 @@ function App() {
       <NavBar />
       <Routes>
         <Route exact path ="/" element={<Home />} />
+        <Route path="/recipedetails" element={<RecipeDetail />} />
+        <Route path="/dietaryrestrictions" element={<DietaryRestrictions />} />
       </Routes>
     </Router>
     </>
--- src/Components/NavBar/NavBar.jsx
+++ src/Components/NavBar/NavBar.jsx
@@ -1,9 +1,35 @@
-import './NavBar.css'
+import React from "react";
+
+import { Link } from "react-router-dom";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faMagnifyingGlass } from "@fortawesome/free-solid-svg-icons";
+
+import logo from "/src/assets/logo.png";
+
+import "./NavBar.css";
 
 export const NavBar = () => {
-    return(
-        <nav>
-            
+    return (
+        <nav className="navbar">
+            <Link to="/">
+                <img src={logo} />
+            </Link>
+            <div className="nav-items">
+                <Link to="/recipedetails" className="links">
+                    Recipe Page
+                </Link>
+                <Link to="/dietaryrestrictions" className="links">
+                    Dietary Restrictions
+                </Link>
+                <div className="search-container">
+                    <FontAwesomeIcon icon={faMagnifyingGlass} />
+                    <input
+                        className="search-input"
+                        type="search"
+                        placeholder="Search"
+                    />
+                </div>
+            </div>
         </nav>
     );
-}
+};
